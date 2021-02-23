b0ef026413f7b06663154ca9a0f2e9ecde6aac33
jjine <kamisuk@naver.com>
2021-02-23 10:17:29 +0900

    add: try 7

---
+++ src/helper.php
@@ -0,0 +1,27 @@
+<?php
+
+function view($name, $data = []) {
+    extract($data);
+    require VIEW . "/$name.php";
+    exit;
+}
+
+function go($url, $msg) {
+    echo "<script>";
+    if(!$msg !== "") 
+    echo "alert('$msg');";
+    echo "location.href='$url';";
+    echo "</script>";
+}
+
+function back($msg) {
+    echo "<script>";
+    if(!$msg !== "") 
+    echo "alert('$msg');";
+    echo "history.back()";
+    echo "</script>";
+}
+
+function user() {
+    return isset($_SESSION['user']) ? $_SESSION['user'] : false;
+}
